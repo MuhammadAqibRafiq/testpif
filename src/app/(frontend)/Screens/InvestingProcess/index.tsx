4b5910c28db5ade191e8a0342c610f6b3dc36c86
muhammadaqibrafiq <aqibrafiq001@outlook.com>
2025-07-01 23:51:31 +0500

    cahnges

--- src/app/(frontend)/Screens/InvestingProcess/index.tsx
+++ src/app/(frontend)/Screens/InvestingProcess/index.tsx
@@ -13,60 +13,70 @@ const Index = () => {
     return (
         <div className="max-w-7xl mx-auto py-12 max-xl:px-6">
 
-            <Typography variant="h2" color="primary" className="mb-4 md:w-1/2 mx-auto" align="center">
+            <Typography variant="h2" color="primary" className="mb-4 max-w-[554px] mx-auto" align="center">
                 Level Up Your Halal Investing Process
             </Typography>
-            <Typography variant="bodyLargeR" color="textSecondary" className="mb-8 md:w-2/3 mx-auto" align="center">
+            <Typography variant="bodyLargeR" color="textSecondary" className="mb-8 max-w-[620px] mx-auto" align="center">
                 Unlock the tools and resources that make ethical, Shariah-compliant finance simple and effective for everyone.
             </Typography>
 
 
             {/* card Section */}
-            <div className="grid md:grid-cols-2 sm:grid-cols-2  gap-8">
+            <div className='flex flex-col gap-[24px]'>
 
-                <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
-                    <div className="mb-6 md:h-[400px] sm:h-[200px]"  >
-                        <Image src={AppleCard} alt="10X Growth" className="w-full h-full" />
+                <div className="grid xl:grid-cols-[576px_1fr] lg:grid-cols-[400px_1fr] sm:grid-cols-2  gap-[24px]">
+
+                    <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
+                        <div className="mb-6 md:h-[400px] sm:h-[200px]"  >
+                            <Image src={AppleCard} alt="10X Growth" className="w-full h-full" />
+                        </div>
+
+                        <Typography variant="h6" className="mb-2 text-primary-50"  >Only invest in halal stock & crypto</Typography>
+                        <Typography variant="bodyMediumR" color="textSecondary" >
+                            Easily find out whether a particular stock or crypto complies with sharia principles. No manual research needed.                    </Typography>
                     </div>
 
-                    <Typography variant="h6" className="mb-2 text-primary-50"  >Only invest in halal stock & crypto</Typography>
-                    <Typography variant="bodyMediumR" color="textSecondary" >
-                        Easily find out whether a particular stock or crypto complies with sharia principles. No manual research needed.                    </Typography>
+                    <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
+                        <div className="mb-6 md:h-[400px] sm:h-[200px]">
+                            <Image src={PieCard} alt="10X Growth" className="w-full h-full" />
+                        </div>
+
+                        <Typography variant="h6" className="mb-2 text-primary-50"  >Expertly-managed halal portfolios</Typography>
+                        <Typography variant="bodyMediumR" color="textSecondary" >
+                            Need some halal investing inspiration? Follow our expertly-managed halal portfolios. We invest our own money into carefully curated stock and crypto portfolios with various return and risk objectives.
+                        </Typography>
+                    </div>
+
+
                 </div>
 
-                <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
-                    <div className="mb-6 md:h-[400px] sm:h-[200px]">
-                        <Image src={PieCard} alt="10X Growth" className="w-full h-full" />
+                <div className="grid xl:grid-cols-[680px_1fr] lg:grid-cols-[580px_1fr] sm:grid-cols-2   gap-[24px]">
+
+                    <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
+                        <div className="mb-6 md:h-[400px] sm:h-[200px]">
+                            <Image src={CryptoTable} alt="10X Growth" className="w-full h-full" />
+                        </div>
+
+                        <Typography variant="h6" className="mb-2 text-primary-50"  >Real-time buy and sell prices</Typography>
+                        <Typography variant="bodyMediumR" color="textSecondary" >
+                            Follow our regularly updated buy and sell prices for hundreds of stocks and cryptocurrencies. No more time spent on calculating the intrinsic value of a stock.
+                        </Typography>
                     </div>
 
-                    <Typography variant="h6" className="mb-2 text-primary-50"  >Expertly-managed halal portfolios</Typography>
-                    <Typography variant="bodyMediumR" color="textSecondary" >
-                        Need some halal investing inspiration? Follow our expertly-managed halal portfolios. We invest our own money into carefully curated stock and crypto portfolios with various return and risk objectives.
-                    </Typography>
-                </div>
+                    <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
+                        <div className="mb-6 md:h-[400px] sm:h-[200px]">
+                            <Image src={nvidiaCard} alt="10X Growth" className="w-full h-full" />
+                        </div>
 
-                <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
-                    <div className="mb-6 md:h-[400px] sm:h-[200px]">
-                        <Image src={CryptoTable} alt="10X Growth" className="w-full h-full" />
+                        <Typography variant="h6" className="mb-2 text-primary-50"  >Reduce the stress of investing</Typography>
+                        <Typography variant="bodyMediumR" color="textSecondary" >
+                            Our skilled analyst insights can help you take the guesswork out of investing in stocks and cryptocurrencies.
+                        </Typography>
                     </div>
-
-                    <Typography variant="h6" className="mb-2 text-primary-50"  >Real-time buy and sell prices</Typography>
-                    <Typography variant="bodyMediumR" color="textSecondary" >
-                        Follow our regularly updated buy and sell prices for hundreds of stocks and cryptocurrencies. No more time spent on calculating the intrinsic value of a stock.
-                    </Typography>
-                </div>
-
-                <div className="bg-primary-10 rounded-2xl shadow padding-lg flex flex-col border border-primary-30">
-                    <div className="mb-6 md:h-[400px] sm:h-[200px]">
-                        <Image src={nvidiaCard} alt="10X Growth" className="w-full h-full" />
-                    </div>
-
-                    <Typography variant="h6" className="mb-2 text-primary-50"  >Reduce the stress of investing</Typography>
-                    <Typography variant="bodyMediumR" color="textSecondary" >
-                        Our skilled analyst insights can help you take the guesswork out of investing in stocks and cryptocurrencies.
-                    </Typography>
                 </div>
             </div>
+
+
 
 
             {/* card Section */}
@@ -81,8 +91,8 @@ const Index = () => {
 
                 <div className="flex flex-col">
                     <div className="md:h-[400px] sm:h-[200px]">
-                            <Image src={Globe} alt="10X Growth"  className="w-full h-full" />
-                        </div>
+                        <Image src={Globe} alt="10X Growth" className="w-full h-full" />
+                    </div>
                 </div>
 
             </div>
