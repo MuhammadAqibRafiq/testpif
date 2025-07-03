1cf4168a4a05bc177c915e93221892b8b3a9a121
muhammadaqibrafiq <aqibrafiq001@outlook.com>
2025-07-04 02:16:07 +0500

    update faqs

--- src/app/(frontend)/Screens/FAQ/index.tsx
+++ src/app/(frontend)/Screens/FAQ/index.tsx
@@ -5,26 +5,26 @@ import Accordion from '@/components/Accordian/Accordian'
 const accordionData = [
     {
         title: "Are returns on my investment guaranteed?",
-        content: "No. All investments carry risk. While PIF Invest uses research-backed, halal-conscious strategies, returns are not guaranteed and your capital is at risk."
+        content: "No. All investments carry risk. While PIF uses research-backed, halal-conscious strategies, returns are not guaranteed and your capital is at risk."
     },
     {
-        title: "Is PIF Invest available outside the United States?",
+        title: "Is PIF available outside the United States?",
         content: "Yes, all you need is a brokerage account. Most of the stocks we invest in are listed on the New York Stock Exchange or Nasdaq—exchanges that nearly all brokerage platforms provide access to."
     },
     {
         title: "How are trades placed? Do I need to have a broker?",
         content: (
             <div className="space-y-2">
-                <p>Yes, you'll need a brokerage account. </p>
+                <p>Yes, you’ll need a brokerage account.</p>
                 <ul className="list-disc list-inside space-y-1 ml-4">
                     <li>Subscribers manually place trades using their own broker.</li>
-                    <li>Advisory clients we do trades on their behalf. This service is not yet open to the public and has limited availability. If you'd like to be notified when spots become available, be sure to join our mailing list.</li>
+                    <li>Advisory clients we do trades on their behalf. This service is not yet open to the public and has limited availability. If you’d like to be notified when spots become available, be sure to join our mailing list.</li>
                 </ul>
             </div>
         )
     },
     {
-        title: "Can I use my existing broker account with PIF Invest?",
+        title: "Can I use my existing broker account with PIF?",
         content: "Subscribers can use any broker they prefer."
     },
     {
@@ -33,11 +33,11 @@ const accordionData = [
     },
     {
         title: "What happens to my investments if I cancel my subscription?",
-        content: "Your investments remain in your account. You'll simply lose access to portfolio updates, trade alerts, our watchlist, expert commentary and investor community."
+        content: "Your investments remain in your account. You’ll simply lose access to portfolio updates, trade alerts, our watchlist, expert commentary and investor community.
     },
     {
         title: "Do you have a referral program?",
-        content: "Yes! Join our referral program to earn commissions by sharing PIF Invest with others. Login to your free account for more information."
+        content: "Yes! Join our referral program to earn commissions by sharing PIF with others. Log in to your free account for more information."
     }
 ];
 
