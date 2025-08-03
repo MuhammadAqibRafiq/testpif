194ad1a53639428d501d447f752b669967694c38
muhammadaqibrafiq <aqibrafiq001@outlook.com>
2025-08-03 13:45:28 +0500

    update key

--- src/lib/instrumentation-client.ts
+++ src/lib/instrumentation-client.ts
@@ -1,7 +1,7 @@
 // instrumentation-client.js
 import posthog from 'posthog-js'
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ||'phc_a0sxhlOIsVcrsF3Tekjgw3NsNiwtMjGEOAFY6nJl9XZ', {
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
     api_host: 'https://us.i.posthog.com',
     defaults: '2025-05-24'
 });
